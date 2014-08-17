47b81e4e8082d78b56e2847c77f3b2a7568a0869
Gergely Imreh <imrehg@gmail.com>
2014-08-17 11:27:05 +0800

    add plotting

--- templogger.py
+++ templogger.py
@@ -1,12 +1,24 @@
+"""
+Temperature logger and plotter
+"""
 import datetime
 import serial
 import logging
+import sys
+import pylab
+
+# For plotting
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
 
 from time import time, strftime
 
 # Set up logging
 logger = logging.getLogger('serialmulti')
-logfilename = "templog_%s.csv" %(strftime("%Y%m%d_%H%M%S"))
+logdate = strftime("%Y%m%d_%H%M%S")
+logfilename = "templog_%s.csv" %(logdate)
 hdlr = logging.FileHandler(logfilename)
 formatter = logging.Formatter('%(message)s')
 hdlr.setFormatter(formatter)
@@ -29,6 +41,37 @@ while True:
             continue
         now = datetime.datetime.now()
         vals = input.strip().split(',')
-        logger.info("%s,%s,%s" % (now,vals[0],vals[1]))
+        temperature, pressure = float(vals[0]), int(vals[1])
+        logger.info("%s,%s,%s" % (now,temperature,pressure))
+        sys.stdout.write("Temperature: %.1f C; Pressure: %.1fhPa   \r" % (temperature,pressure/1000.0) )
+        sys.stdout.flush()
     except (KeyboardInterrupt):
         break
+    except (ValueError):
+        continue
+
+
+## Do the plotting
+
+timestamp, temperature, pressure = np.loadtxt(logfilename,
+                                              unpack=True,
+                                              delimiter=",",
+                                              converters={ 0: mdates.strpdate2num("%Y-%m-%d %H:%M:%S.%f")})
+
+fig = plt.figure(figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')
+ax = fig.add_subplot(111)
+
+# ax.set_xticks(timestamp) # Tickmark + label at every plotted point
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+
+plt.plot_date(x=timestamp, y=temperature, fmt="k-")
+plt.title("Temperature record from %s" %(logfilename))
+plt.ylabel("Temperature")
+plt.xlabel("Time")
+plt.ylim([min(temperature)-0.2, max(temperature)+0.2])
+fig.autofmt_xdate(rotation=45)
+plt.grid(True)
+imagefilename = "plot_%s.png" %(logdate)
+plt.savefig(imagefilename)
+print("\n\n...Image saved to %s" %(imagefilename))
+plt.show()
